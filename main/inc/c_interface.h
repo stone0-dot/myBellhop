4ad6aabcdcc5922f560ac17b38f626b9b978cad9
stone <564609719@qq.com>
2023-04-20 09:56:13 +0800

    问题：链接动态库重定义

--- main/inc/c_interface.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-int add_interface /* WARNING: Converting 'INTEGER(4)' to interoperable type */ (int *a /* WARNING: non-interoperable KIND */ , int *b /* WARNING: non-interoperable KIND */ );
-int foo_interface /* WARNING: Converting 'INTEGER(4)' to interoperable type */ ();
-int helloworld_interface /* WARNING: Converting 'INTEGER(4)' to interoperable type */ ();
